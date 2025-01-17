7430ae1b8f70ec26d473488bbcf0c1f90de7cb19
hudson <hudson.arruda@outlook.com>
2025-01-17 08:42:14 -0300

    🐛 [fix]: Fix SignIn Route

--- src/routes/MainRoute.tsx
+++ src/routes/MainRoute.tsx
@@ -29,7 +29,7 @@ const LogoutButton = (props: TouchableOpacityProps) => {
 const MainRoutes = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home"  >
+      <Stack.Navigator initialRouteName="SignIn"  >
         <Stack.Screen name="Home" component={Home}
           options={({ navigation }) => ({
             headerBackVisible: false,
